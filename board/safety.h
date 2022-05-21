d14c8305446a1a538506a37b13b05a5279cfb4c9
redacid95 <jonathanc.olivier@gmail.com>
2022-05-21 14:23:54 -0700

    Revert "test"
    This reverts commit 08b6a3058e520e095e414302caa3d02f7e4ee6da.

--- board/safety.h
+++ board/safety.h
@@ -76,7 +76,7 @@ int safety_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 }
 
 bool get_longitudinal_allowed(void) {
-  return controls_allowed;
+  return controls_allowed && !gas_pressed_prev;
 }
 
 // Given a CRC-8 poly, generate a static lookup table to use with a fast CRC-8
